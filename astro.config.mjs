5355ba7653516f717dc37b132a1fcf5c23462ed3
Gaxve <adrivegadev@gmail.com>
2024-04-24 02:39:05 +0200

    netlify setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import lottie from "astro-integration-lottie";
+import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [icon(), mdx(), lottie()],
   output: "server",
   site: "https://keithslyffer.github.io",
+  adapter: netlify()
 });
